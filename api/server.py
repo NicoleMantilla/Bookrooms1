ab26c5aabd7010b78d729bf67b69f4396bb80fa0
John Doe <nicolemf14@gmail.com>
2024-04-14 17:12:04 +0200

    backend server.

---
+++ api/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+room_list = [
+    {
+        "room_key": 1,
+        "room_name": "Habitación doble Vista Jardín",
+        "room_description": "Dos camas individuales, suelos de madera, balcón con vistas, plato de ducha, accesorios de baño, WiFi, secador de pelo, TV 32’’, calefacción, aire acondicionado, caja fuerte y mini bar con coste adicional.",
+        "room_picture": "hab_vista_jardin.png",
+        "available_occupancy": ["1-0-0", "1-1-0", "2-0-0"],
+        "data_prices": [
+            [{"target_occupancy": ["1-0-0"], "rate_id": "NRF", "rate_name": "Tarifa No Reembolsable", "price": 200.00}, {"rate_id": "FLEX", "rate_name": "Tarifa Flexible", "price": 250.00}],
+            [{"target_occupancy": ["1-1-0", "2-0-0"], "rate_id": "NRF", "rate_name": "Tarifa No Reembolsable", "price": 300.00}, {"rate_id": "FLEX", "rate_name": "Tarifa Flexible", "price": 350.00}]
+        ]
+    },
+    {
+        "room_key": 2,
+        "room_name": "Habitación doble Vista Mar",
+        "room_description": "Maravillosas vistas al mar mediterráneo. Habitación con suelos de madera, plato de ducha, jacuzzi hidromasaje, amenities y secador de pelo. WiFi, TV 42’, calefacción, aire acondicionado, caja fuerte y mini bar sin coste adicional.",
+        "room_picture": "hab_vista_mar.png",
+        "available_occupancy": ["1-0-0", "2-0-0"],
+        "data_prices": [
+            [{"target_occupancy": ["1-0-0"], "rate_id": "NRF", "rate_name": "Tarifa No Reembolsable", "price": 400.00}, {"rate_id": "FLEX", "rate_name": "Tarifa Flexible", "price": 450.00}],
+            [{"target_occupancy": ["2-0-0"], "rate_id": "NRF", "rate_name": "Tarifa No Reembolsable", "price": 550.00}, {"rate_id": "FLEX", "rate_name": "Tarifa Flexible", "price": 600.00}]
+        ]
+    }
+]
+
+
+@app.route('/', methods=['GET'])
+def index():
+	return jsonify({'rooms': room_list})
+
+if __name__ == '__main__':
+	app.run(debug=True)

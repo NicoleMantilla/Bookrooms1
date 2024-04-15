9820c3aacee85e60ca0b31db2c6f296e70630b05
John Doe <nicolemf14@gmail.com>
2024-04-15 23:56:57 +0200

    update: Navbar.

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li><a href="#habitaciones">Habitaciones</a></li>
-        <li><a href="#formulario">Formulario</a></li>
+        <li><a href="#habitaciones">Inicio</a></li>
+        <li><a href="#formulario">Buscador</a></li>
       </ul>
     </nav>
   );

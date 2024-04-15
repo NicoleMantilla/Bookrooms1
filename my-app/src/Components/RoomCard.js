5d071d7dbe2fe512da79074a2acffe07f1126250
John Doe <nicolemf14@gmail.com>
2024-04-15 20:46:46 +0200

    update: Style Sass.

--- my-app/src/Components/RoomCard.js
+++ my-app/src/Components/RoomCard.js
@@ -2,29 +2,33 @@ import React from 'react';
 
 function RoomCard({ room }) {
   return (
-    <figure className="image-block">
-      <img src={`/images/${room.room_picture}`} alt={room.room_name} />
-      <div className="room-details">
+    <figure className="room-card">
+      <img src={`/images/${room.room_picture}`} alt={room.room_name} className="room-image" />
+      <div className="room-caption">
         <h1>{room.room_name}</h1>
-        <figcaption>
-        <p>{room.room_description}</p>
-        <ul>
-          {room.available_occupancy.map((occupancy, index) => (
-            <li key={index}>{occupancy}</li>
-          ))}
-        </ul>
-        
-        <h3>Precios:</h3>
-        {room.data_prices.map((priceGroup, index) => (
-          <div key={index}>
-            {priceGroup.map((price, index) => (
-              <div key={index}>
-                <p>{price.rate_name}: ${price.price}</p>
+      </div>
+      <div className="room-overlay">
+        <div className="room-details">
+          <p className="room-description">{room.room_description}</p>
+          <div className="room-availability">
+            <h3>Disponibilidad:</h3>
+            <ul>
+              {room.available_occupancy.map((occupancy, index) => (
+                <li key={index}>{occupancy}</li>
+              ))}
+            </ul>
+          </div>        
+          <div className="room-prices">
+            <h3>Precios:</h3>
+            {room.data_prices.map((priceGroup, index) => (
+              <div key={index} className="price-group">
+                {priceGroup.map((price, index) => (
+                  <p key={index}>{price.rate_name}: ${price.price}</p>
+                ))}
               </div>
             ))}
           </div>
-        ))}
-        </figcaption>
+        </div>
       </div>
     </figure>
   );

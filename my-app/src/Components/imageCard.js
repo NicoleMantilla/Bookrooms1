653ace04d20f7acdf0cda60da50ffad31ed9e4f9
John Doe <nicolemf14@gmail.com>
2024-04-15 02:09:21 +0200

    update: images.

--- my-app/src/Components/imageCard.js
+++ my-app/src/Components/imageCard.js
@@ -1,7 +1,5 @@
-// App.js en el frontend
-
 import React from 'react';
-import './App.css'; // Asegúrate de tener los estilos de tu aplicación si los necesitas
+
 
 const imageUrl = 'http://localhost:5000/static/images/image1.jpg';
 
